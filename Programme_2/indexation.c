a5559431457da440670f2c4df82d10ac8b62d5de
Romaiiin91 <chevalier.romain91@gmail.com>
2021-12-15 10:53:48 +0100

    signature

--- Programme_2/indexation.c
+++ Programme_2/indexation.c
@@ -4,11 +4,12 @@
 #include <sys/stat.h>
 
 //#define CLEAR2CONTINUE
-#include "include/traces.h" 
+#include "../include/traces.h" 
 
 // C'est dans le fichier elt.h qu'on doit choisir l'implémentation des T_elt
 #include "elt.h"
 #include "avl.h"
+#include"indexation.h"
 
 int main(int argc, char ** argv) {
 	T_avl root = NULL; 
@@ -18,7 +19,7 @@ int main(int argc, char ** argv) {
 	/////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////
 
-	outputPath = "output_Prog2";
+	/* outputPath = "output_Prog2";
 	
 	char *filename;
 	
@@ -43,7 +44,62 @@ int main(int argc, char ** argv) {
     }
 
     fclose(in_file);
-    printAVL(root,0);
+    printAVL(root,0); */
+
+    char *mot ="coucou";
+    int taille = strlen(mot);
+    char *sign;
+
+    sign = cal_signature(mot, taille);
+    printf("%s\n", sign);
 
 	return 0;
+}
+
+char * cal_signature(char *mot, int taille){
+    char * sign;
+    
+    sign = (char*) malloc(sizeof(mot));
+    memcpy(sign, mot, taille);
+    mergeSort_tab(sign, 0, taille-1); // On ne trie pas le caractère de fin \0
+
+    return sign;
+}
+
+
+     
+
+void mergeSort_tab(char t[], int debut, int fin){
+	int milieu;
+
+	if (debut < fin)
+	{
+		milieu = (debut + fin)/2;
+		mergeSort_tab(t, debut, milieu);
+		mergeSort_tab(t, milieu + 1, fin);
+		fusionner_tab(t, debut, milieu, fin);
+	}
+}
+
+
+void fusionner_tab(char t[], int d, int m, int f){
+	char aux[f - d + 1]; // !! Allocation dynamique sur la pile (standard C99) 
+	int i, j, k;
+	
+
+	memcpy(aux, &t[d], (f - d + 1) * sizeof(char));	// Copie des données à fusionner
+	
+
+	i = 0; j = m - d + 1; k = 0;
+	while (i <= m - d && j <= f - d) {
+		
+		if (aux[i] <= aux[j]) 	{
+			t[d + k++] = aux[i++];	// aux[i] est plus petit : on le place dans t 
+		}
+		else {
+	 		t[d + k++] = aux[j++];	// aux[j] est plus petit : on le place dans t 
+		}
+	}
+	for (; i <= m - d; t[d + k++] = aux[i++]); // le reste du tableau gauche
+	for (; j <= f - d; t[d + k++] = aux[j++]); // le reste du tableau droit
 }
--- Programme_2/indexation.h
+++ Programme_2/indexation.h
@@ -1,3 +1,6 @@
 #include "elt.h" // T_elt 
 #include"avl.h"
 
+char * cal_signature(char *mot, int taille);
+void mergeSort_tab(char t[], int debut, int fin);
+void fusionner_tab(char t[], int d, int m, int f);
