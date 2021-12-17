91e7b82cc53f7d6b60d6cc6e16ad5d4590c425a8
Romaiiin91 <chevalier.romain91@gmail.com>
2021-12-17 19:01:42 +0100

    v4

--- Programme_2/indexation.c
+++ Programme_2/indexation.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <sys/stat.h>
+#include <time.h> 
 
 //#define CLEAR2CONTINUE
 #include "../include/traces.h" 
@@ -14,7 +15,7 @@
 
 int main(int argc, char ** argv) {
 	T_avl root = NULL; 
-	T_avlNode * pAux = NULL;
+	
 
     
 	/////////////////////////////////////////////////////////////////////////
@@ -40,6 +41,10 @@ int main(int argc, char ** argv) {
     //Initialisation paramètres à afficher
     long int compteur_mots =0;
     int taille_mots=strlen(file_contents);
+    clock_t start = clock(), stop;
+    int hauteur, nb_noeud;
+    int nb_noeud_temp, h_min=1; // On initialise h_min pour la hauteur minimale contenant le même nombre de noeud à 1 pour compter le premier noeud
+    float duree;
 
 
     //Ajout des mots à l'arbre
@@ -48,23 +53,33 @@ int main(int argc, char ** argv) {
     insertAVL(&root, file_contents, taille_mots);
     
     taille_mots = strlen(file_contents);
+    
 
     while (fscanf(in_file, "%[^\n] ", file_contents) != EOF){
         insertAVL(&root, file_contents, taille_mots);
         compteur_mots++;
-
-        
     }
 
+    stop = clock();
+
     fclose(in_file);
-    //printAVL(root,0); 
-    printf("Taille des mots : %d\nNombre de mots: %ld\n", taille_mots, compteur_mots);
     
+    
+    // Paramètres de l'arbre
+    duree = (stop-start)* 1000.0 / CLOCKS_PER_SEC;
+    nb_noeud = nbNodesAVL(root);
+    hauteur = heightAVL(root);
 
+    nb_noeud_temp = nb_noeud;
+    while ((nb_noeud_temp/=2)>0) h_min++;
 
+    printf("Taille des mots : %d\nNombre de mots: %ld\nDurée de construction: %.2f \n"
+        "Nombre de noeuds: %d\nHauteur: %d\nHauteur minimale d’un arbre contenant %ld noeuds: %d\n",
+        taille_mots, compteur_mots, duree, nb_noeud, hauteur, nb_noeud, h_min
+        );
   
     //printf("%s et %d\n", file_contents, strlen(file_contents)* sizeof(char*));
-  
+
     
 
 	return 0;
