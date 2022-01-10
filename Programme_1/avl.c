4bc096f11c2935c9ada52f438e0c17a115bb8864
Romaiiin91 <chevalier.romain91@gmail.com>
2022-01-10 13:37:09 +0100

    CR v3.2

--- Programme_1/avl.c
+++ Programme_1/avl.c
@@ -244,29 +244,29 @@ static void  genDotAVL(T_avl root, FILE *fp) {
 	// => on ne peut pas faire deux appels à toString dans le même printf()
 
     fprintf(fp, "\t\"%s\"",toString(root->val)); 
-    fprintf(fp, " [label = \"{{<g> %s | <d> %d } | { <g> | <d>}}\"];\n",toString(root->val), root->bal);
+    fprintf(fp, " [label = \"{{<nom> %s | <bal> %d } | { <g> | <d>}}\"];\n",toString(root->val), root->bal);
     if (root->r == NULL && root->l == NULL) {
         fprintf(fp, "\t\"%s\"", toString(root->val)); 
-		fprintf(fp, " [label = \"{{<g> %s | <d> %d }| { <g> NULL | <d> NULL}}\"];\n", toString(root->val), root->bal);
+		fprintf(fp, " [label = \"{{<nom> %s | <bal> %d }| { <g> NULL | <d> NULL}}\"];\n", toString(root->val), root->bal);
 	}
     else if (root->r == NULL) {
         fprintf(fp, "\t\"%s\"", toString(root->val));
-		fprintf(fp, " [label = \"{{<g> %s | <d> %d }| { <g> | <d> NULL}}\"];\n", toString(root->val), root->bal);
+		fprintf(fp, " [label = \"{{<nom> %s | <bal> %d }| { <g> | <d> NULL}}\"];\n", toString(root->val), root->bal);
 	}
 	else if ( root->l == NULL) {
         fprintf(fp, "\t\"%s\"",toString(root->val));
-		fprintf(fp, " [label = \"{{<g> %s | <d> %d }| { <g> NULL | <d> }}\"];\n", toString(root->val), root->bal);
+		fprintf(fp, " [label = \"{{<nom> %s | <bal> %d }| { <g> NULL | <d> }}\"];\n", toString(root->val), root->bal);
 	}
 	
     if (root->l) {
         fprintf(fp, "\t\"%s\"",toString(root->val));
-		fprintf(fp, ":g -> \"%s\";\n", toString(root->l->val));
+		fprintf(fp, ":g -> \"%s\":nom;\n", toString(root->l->val));
         genDotAVL(root->l, fp);
     }
 
     if (root->r) {
         fprintf(fp, "\t\"%s\"",toString(root->val));
-		fprintf(fp,":d -> \"%s\";\n", toString(root->r->val));
+		fprintf(fp,":d -> \"%s\":nom;\n", toString(root->r->val));
         genDotAVL(root->r, fp);
     }
 }
